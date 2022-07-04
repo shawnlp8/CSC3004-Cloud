94c1f482b150b82afb202fa119820ea94977db00
shawnlp8 <shawnliow97@gmail.com>
2022-07-04 21:01:36 +0800

    Removed gRPC and modified database

--- Flask/app.py
+++ Flask/app.py
@@ -7,23 +7,10 @@ import os
 from urllib import response
 from flask import Flask, render_template, json, Response, redirect, redirect, jsonify
 
-# gRPC Imports
-import grpc
-import item_locator_pb2
-import item_locator_pb2_grpc
-from concurrent import futures
-import logging
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = item_locator_pb2_grpc.ItemLocatorStub(channel)
-        response = stub.SearchItem(item_locator_pb2.Request(name='test'))
-        print("\n[!] gRPC Response: ", str(response.res), "\n")
-    
     return render_template('index.html')
 
 if __name__ == '__main__':
