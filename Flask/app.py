eaa58392ead1220d9ea83fca5fb9db9156bc62f9
shawnlp8 <shawnliow97@gmail.com>
2022-07-04 22:54:51 +0800

    Added simple SQLite3 search function

--- Flask/app.py
+++ Flask/app.py
@@ -5,17 +5,37 @@ import random
 import datetime
 import os
 from urllib import response
-from flask import Flask, render_template, json, Response, redirect, redirect, jsonify
+from flask import Flask, render_template, json, Response, redirect, redirect, jsonify, request
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    searchItemHelper()
     return render_template('index.html')
+
+@app.route('/searchItem', methods = ["GET"])
+def searchItem():
+    if (request.method == "GET"):
+        item = request.form.get("item")
+        searchItemHelper(item)
+        
+# Helper functions
+def searchItemHelper():
+    connection = sqlite3.connect('../database/database.db')
+    cursor = connection.cursor()
+    sqlStatement = "SELECT * FROM Product WHERE name LIKE '%'" + str(item) + "'%'"
+    cursor.execute(sqlStatement)
+    record = cursor.fetchall()
+    cursor.close()
+    print(record)
+    return record
+        
 
 if __name__ == '__main__':
     # Run the Flask server
-    print("Flask server started and listening on port 5000...\n")
+    print("\nFlask server started and listening on port 5000...\n")
     app.run(debug=True)
     
     
